e7676d42d283ceb7797cd312b0b4e9fe0360ba95
justinbscanlon <justinbscanlon@gmail.com>
2020-06-24 11:12:16 +0100

    Add project files.

---
+++ SampleRER/Service1.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Microsoft.SharePoint.Client.EventReceivers;
+
+namespace SampleRER
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IRemoteEventService
+    {
+        
+        public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+        {
+            var props = properties;
+            throw new NotImplementedException();
+        }
+    }
+}
